5070bfca616ca2497926ad076877975a4f9e86b9
hoccodec <111237570+hoccodec@users.noreply.github.com>
2025-03-09 11:30:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function checkPassword(imageSrc, button) {
+    const password = prompt("Nhập mật khẩu để xem hình:");
+    const correctPassword = "123456"; // Mật khẩu đúng
+
+    if (password === correctPassword) {
+        alert("Mật khẩu đúng! Bạn có thể xem hình.");
+        showImage(button.parentElement.querySelector('img'), imageSrc); // Hiển thị hình ảnh
+    } else {
+        alert("Mật khẩu sai! Vui lòng thử lại.");
+    }
+}
+
+function showImage(imgElement, src) {
+    imgElement.classList.remove('blurred'); // Xóa lớp làm mờ
+    showFullscreenImage(src); // Hiển thị hình ảnh toàn màn
+    }
+    
+    function showFullscreenImage(src) {
+        const fullscreenImage = document.getElementById('fullscreenImage');
+        const fullscreenImg = document.getElementById('fullscreenImg');
+        fullscreenImg.src = src; // Lấy src của hình ảnh
+        fullscreenImage.style.display = 'flex'; // Hiển thị hình ảnh toàn màn hình
+    }
+    
+    function hideFullscreen() {
+        const fullscreenImage = document.getElementById('fullscreenImage');
+        fullscreenImage.style.display = 'none'; // Ẩn hình ảnh toàn màn hình
+    }
